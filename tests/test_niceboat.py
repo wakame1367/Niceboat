4a524a1b86e3dd5620144a7026046d39d633eabc
wakamezake <hotwater1367@gmail.com>
2020-09-07 01:42:27 +0900

    add test case

--- tests/test_niceboat.py
+++ tests/test_niceboat.py
@@ -1,17 +1,38 @@
-import pytest
+from pathlib import Path
+
 import pandas as pd
+import pytest
+
 from niceboat import RaceResult, BoatRaceParser
+
+test_data_path = Path('tests/data/')
 
 
 @pytest.fixture
-def test_data():
-    with open('tests/data/K200901.TXT', 'r', encoding='cp932') as f:
+def normal_case():
+    return open_txt_file(test_data_path / 'K200901.TXT')
+
+
+@pytest.fixture()
+def exception_case():
+    return open_txt_file(test_data_path / 'ex_K200901.TXT')
+
+
+def open_txt_file(path):
+    with open(path, 'r', encoding='cp932') as f:
         txt = f.read()
     return txt
 
 
-def test_parser(test_data):
-    txt = test_data
+def test_parser(normal_case):
+    txt = normal_case
+    parser = BoatRaceParser(rule=RaceResult)
+    parsed_txt = parser.parse(txt)
+    assert isinstance(parsed_txt, pd.DataFrame)
+
+
+def test_parser_exception_pattern(exception_case):
+    txt = exception_case
     parser = BoatRaceParser(rule=RaceResult)
     parsed_txt = parser.parse(txt)
     assert isinstance(parsed_txt, pd.DataFrame)
